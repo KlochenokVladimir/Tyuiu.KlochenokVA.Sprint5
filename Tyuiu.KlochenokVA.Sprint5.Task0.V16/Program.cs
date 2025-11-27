4ebe21ad42ca32314b7a1944fe166c742daec99d
Владимир Клоченок <Tyuiu.KlochenokV@outlook.com>
2025-11-27 20:04:45 +0500

    Add project files.

---
+++ Tyuiu.KlochenokVA.Sprint5.Task0.V16/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.KlochenokVA.Sprint5.Task0.V16.Lib;
+
+namespace Tyuiu.KlochenokVA.Sprint5.Task0.V16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            
+            Console.Title = "Спринт #5 | Выполнила: Клоченок В. А. | ИИПб-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                       *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнила: Клоченок Виктория Андреевна | ИИПб-24-2                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение y(x) = (2x² - 1) / √(x² - 2)                           *");
+            Console.WriteLine("* вычислить его значение при x = 3, результат сохранить в текстовый      *");
+            Console.WriteLine("* файл OutPutFileTask0.txt и вывести на консоль. Округлить до трёх       *");
+            Console.WriteLine("* знаков после запятой.                                                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            
+            int x = 3;
+            Console.WriteLine("x = " + x);
+            
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            
+            string res = ds.SaveToFileTextData(x);
+            
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
