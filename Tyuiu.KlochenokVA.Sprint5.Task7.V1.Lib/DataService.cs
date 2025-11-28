c48adac98701ad9ff4b1a4c60d644f5c790b5e84
Владимир Клоченок <Tyuiu.KlochenokV@outlook.com>
2025-11-28 18:57:34 +0500

    а

--- Tyuiu.KlochenokVA.Sprint5.Task7.V1.Lib/DataService.cs
+++ Tyuiu.KlochenokVA.Sprint5.Task7.V1.Lib/DataService.cs
@@ -4,11 +4,6 @@
     {
         public string LoadDataAndSave(string path)
         {
-            if (!File.Exists(path))
-            {
-                throw new FileNotFoundException($"Файл не найден: {path}");
-            }
-
             string content = File.ReadAllText(path);
             string result = "";
 
@@ -18,12 +13,6 @@
                 {
                     result += c;
                 }
-            }
-
-            result = result.Replace("  ", " ").Trim();
-            if (result.EndsWith(" ."))
-            {
-                result = result.Substring(0, result.Length - 2);
             }
 
             string outputPath = Path.Combine(Path.GetTempPath(), "OutPutDataFileTask7V1.txt");
