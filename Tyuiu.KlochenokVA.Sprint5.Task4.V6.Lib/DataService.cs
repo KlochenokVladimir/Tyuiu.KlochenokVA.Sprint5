3bbb4012baea397c1d22e416a1d3cc225643d648
Владимир Клоченок <Tyuiu.KlochenokV@outlook.com>
2025-11-28 18:08:05 +0500

    4

---
+++ Tyuiu.KlochenokVA.Sprint5.Task4.V6.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.KlochenokVA.Sprint5.Task4.V6.Lib
+{
+    public class DataService : ISprint5Task4V6
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strX = File.ReadAllText(path);
+            double x = Convert.ToDouble(strX);
+            double res = Math.Round((1.0 / Math.Cos(x)) + 2.2 * Math.Pow(x, 2), 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.KlochenokVA.Sprint5.Task4.V6/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.KlochenokVA.Sprint5.Task4.V6.Lib;
+
+namespace Tyuiu.KlochenokVA.Sprint5.Task4.V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Клоченок В. А. | ИСПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение данных из файла                                           *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнил: Клоченок Владимир Алексеевич | ИСПб-25-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение y = 1/cos(x) + 2.2x²                                   *");
+            Console.WriteLine("* Прочитать значение из файла и подставить вместо x в формулу.           *");
+            Console.WriteLine("* Округлить до трёх знаков после запятой.                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = Path.Combine(Path.GetTempPath(), "InPutDataFileTask4V6.txt");
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
